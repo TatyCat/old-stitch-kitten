6b721df0d4d8e6ab9362638b4b305188d606ee99
TatyCat <williams.tatyanna@gmail.com>
2018-05-12 22:13:24 -0400

    Added Twitter Link

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+// Blank Slate upon load, remove header/title
+window.onload = function(){
+    document.getElementById('patternTitle').style.visibility = "hidden";
+}
+
+
+// Click Add Button To Add Pattern
+// Upon successful 1st step add, show the patternTitle again #visible
+
+
+//Click Row Counter Button To Count the Stitch for the Pattern Step
+
+
+// Close Counter Button Upon CLick Event
+
+
+// Click Delete Step Button
+
+
+// Delete the Current Pattern
